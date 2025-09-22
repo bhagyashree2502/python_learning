8ff102d59488efffa2533e8f7c4b1281bacf2cd3
Bhagyashree <khatribhagyashree370@gmail.com>
2025-09-22 17:35:51 +0530

    cdfda

---
+++ day1.py
@@ -0,0 +1,13 @@
+#pyhton_learning
+print("Hello world")
+
+# taking an input from user
+# the input function by default take input as a string
+a = input("Enter your name: ")  #a store the input value
+print(a)
+
+# taking a numerical input value
+# >>>use int() method
+
+b = int(input("Enter a number: "))
+print(b)
