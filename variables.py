b3e3efc5f5348cedba07e42d7fac77efbeeeed96
Bhagyashree <khatribhagyashree370@gmail.com>
2025-09-23 15:15:11 +0530

    variable

---
+++ variables.py
@@ -0,0 +1,7 @@
+# variables are the containers that contains a value.
+# this value can be numerical, characters, strings, arrays, etc.
+# a variable contains a integer value:
+a = 5
+
+# b variable contains a string:
+b = "bhagyashree"
