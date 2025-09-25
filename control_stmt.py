b87b1862a6e6c7c932f664d438510a223c0180b5
Bhagyashree <khatribhagyashree370@gmail.com>
2025-09-25 11:06:20 +0530

    conditional statements

---
+++ control_stmt.py
@@ -0,0 +1,17 @@
+#******CONTROL STATEMENTS*****************
+# Control statements are used to control the flow of execution in a program.
+# They help you decide what to execute, how many times, and when to skip or stop certain parts of code.
+# Conditional statements >>(if,elif,else)
+# if – checks the first condition
+
+# elif – checks more conditions if previous ones are false
+
+# else – runs when no conditions are true
+
+# a program to check whether a person is adult or not
+age = 18
+
+if age >= 18:
+    print("You are an adult.")  # Runs if condition is True
+else:
+    print("You are a minor.")   # Runs if condition is False
