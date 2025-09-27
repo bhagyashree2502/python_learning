4494f99eca84ac1d117bf2e5b09f6bc7d20a5536
Bhagyashree <khatribhagyashree370@gmail.com>
2025-09-27 17:39:42 +0530

    prime

---
+++ prime_number.py
@@ -0,0 +1,9 @@
+#**************PRIME NUMBER OR NOT *****************
+# n = int (input("enter the number: "))
+# for i in range(2,n):
+#     # if(i%1 == 0 and i%i==0 and i%n)
+#     if(n%i == 0):
+#         print("Number is not prime")
+#         break
+# else:
+#     print("Number is prime")
